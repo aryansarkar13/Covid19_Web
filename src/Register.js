5d6b96cb546a5f232a9a6ad2db84afe68ec99c3c
josh <srijandasbiswas7540@gmail.com>
2020-03-28 19:38:09 +0530

    registration-form

---
+++ src/Register.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+    render() {
+        return (
+            <div className="card" style={{width:300, marginLeft:400,backgroundColor:"#5699ba"}}>
+            <div className="card-title">
+
+            </div>
+            <div className="card-body">
+             
+
+            <form>
+            <div class="form-group">
+    <label for="name1" style={{color:"#fff"}}>Name</label>
+    <input type="text" class="form-control" id="name1" placeholder="Enter Name"/>
+  </div>
+  <div class="form-group">
+    <label for="email1" style={{color:"#fff"}}>Email address</label>
+    <input type="email" class="form-control" id="email1" placeholder="name@example.com"/>
+  </div>
+  <div class="form-group">
+    <label for="contact1" style={{color:"#fff"}}>Contact</label>
+    <input type="number" class="form-control" id="contact1" placeholder="Enter Contact"/>
+  </div>
+  <div class="form-group">
+    <label for="Select1" style={{color:"#fff"}}>Status</label>
+    <select class="form-control" id="Select1">
+      <option>1. Corona Virus Positive</option>
+      <option>2.Shows Symtoms</option>
+      <option>3. Travel History Abroad</option>
+      <option>4. Close Contact</option>
+      <option>5. Normal User</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="lat1" style={{color:"#fff"}}>Latitude</label>
+    <input type="string" class="form-control" id="lat1" placeholder="Enter Latitude"/>
+  </div>
+  <div class="form-group">
+    <label for="long1" style={{color:"#fff"}}>Longitude</label>
+    <input type="string" class="form-control" id="long1" placeholder="Enter Longitude"/>
+  </div>
+  
+  
+</form>
+
+
+            </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Register;
